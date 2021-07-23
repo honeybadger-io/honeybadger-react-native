5d677d030339612e22c4cb13b5ca72fd7354663e
Matei Oprea <eu@opreamatei.ro>
2021-07-23 16:39:10 +0300

    Modify generate sourcemaps script
    Modify generate sourcemaps script in order to work with typescript files.

--- honeybadger-generate-sourcemaps.sh
+++ honeybadger-generate-sourcemaps.sh
@@ -13,11 +13,27 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT_DIR="$SCRIPT_DIR/../../"
+INDEX_FILE="$PROJECT_ROOT_DIR/index.js"
+
+# Check if index.js file exists and if try to search for .ts or .tsx equivalent
+
+if [[ ! -f "$INDEX_FILE" ]]; then
+	if [[ -f "$PROJECT_ROOT_DIR/index.tsx" ]]; then
+		INDEX_FILE="$PROJECT_ROOT_DIR/index.tsx"
+	else
+		if [[ -f "index.ts" ]]; then
+			INDEX_FILE="$PROJECT_ROOT_DIR/index.ts"
+		else
+			echo "Couldn't find index file"
+			exit 1
+		fi
+	fi
+fi
 
 echo "Generating source maps for iOS ..."
-npx react-native bundle --platform ios --entry-file "$PROJECT_ROOT_DIR/index.js" --dev false --reset-cache --bundle-output main.jsbundle-ios --assets-dest /dev/null --sourcemap-output sourcemap-ios --sourcemap-sources-root "PROJECT_ROOT_DIR" > /dev/null 2>&1
+npx react-native bundle --platform ios --entry-file "$INDEX_FILE" --dev false --reset-cache --bundle-output main.jsbundle-ios --assets-dest /dev/null --sourcemap-output sourcemap-ios --sourcemap-sources-root "PROJECT_ROOT_DIR" > /dev/null 2>&1
 
 echo "Generating source maps for Android ..."
-npx react-native bundle --platform android --entry-file "$PROJECT_ROOT_DIR/index.js" --dev false --reset-cache --bundle-output main.jsbundle-android --assets-dest /dev/null --sourcemap-output sourcemap-android --sourcemap-sources-root "PROJECT_ROOT_DIR" > /dev/null 2>&1
+npx react-native bundle --platform android --entry-file "$INDEX_FILE" --dev false --reset-cache --bundle-output main.jsbundle-android --assets-dest /dev/null --sourcemap-output sourcemap-android --sourcemap-sources-root "PROJECT_ROOT_DIR" > /dev/null 2>&1
 
 echo "Done. Your project root directory now contains sourcemap-ios, sourcemap-android, main.jsbundle-ios, and main.jsbundle-android."
